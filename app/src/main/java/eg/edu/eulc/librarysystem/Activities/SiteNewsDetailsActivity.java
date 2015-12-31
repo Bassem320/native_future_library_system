0090d4023c450f99ca39c2468ab2cc34631c430b
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2015-12-31 14:31:17 +0200

    Enhance Site News details

--- app/src/main/java/eg/edu/eulc/librarysystem/Activities/SiteNewsDetailsActivity.java
+++ app/src/main/java/eg/edu/eulc/librarysystem/Activities/SiteNewsDetailsActivity.java
@@ -1,12 +1,19 @@
 package eg.edu.eulc.librarysystem.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.style.ClickableSpan;
+import android.text.style.URLSpan;
+import android.view.View;
 import android.widget.TextView;
 
-import eg.edu.eulc.librarysystem.R;
 import eg.edu.eulc.librarysystem.Objects.SiteNewsItem;
+import eg.edu.eulc.librarysystem.R;
 
 public class SiteNewsDetailsActivity extends AppCompatActivity {
 
@@ -24,11 +31,35 @@ public class SiteNewsDetailsActivity extends AppCompatActivity {
         details = (TextView) findViewById(R.id.siteNewsDetails);
 
         Bundle bundle = SiteNewsDetailsActivity.this.getIntent().getExtras();
-        if(bundle!=null) {
+        if (bundle != null) {
             SiteNewsItem item = bundle.getParcelable("SiteNewsItem");
             title.setText(item.getTitle());
-            details.setText(item.getDetails());
+            setTextViewHTML(details, item.getDetails());
         }
     }
 
+    protected void makeLinkClickable(SpannableStringBuilder strBuilder, final URLSpan span) {
+        int start = strBuilder.getSpanStart(span);
+        int end = strBuilder.getSpanEnd(span);
+        int flags = strBuilder.getSpanFlags(span);
+        ClickableSpan clickable = new ClickableSpan() {
+            public void onClick(View view) {
+                // Do something with span.getURL() to handle the link click...
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(span.getURL()));
+                startActivity(browserIntent);
+            }
+        };
+        strBuilder.setSpan(clickable, start, end, flags);
+        strBuilder.removeSpan(span);
+    }
+
+    protected void setTextViewHTML(TextView text, String html) {
+        CharSequence sequence = Html.fromHtml(html);
+        SpannableStringBuilder strBuilder = new SpannableStringBuilder(sequence);
+        URLSpan[] urls = strBuilder.getSpans(0, sequence.length(), URLSpan.class);
+        for (URLSpan span : urls) {
+            makeLinkClickable(strBuilder, span);
+        }
+        text.setText(strBuilder);
+    }
 }
