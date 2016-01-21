0062ecf0a9f6cee397d946d46aea617817acb74d
Eslam El-Meniawy <eslam.elmeniawy@gmail.com>
2016-01-21 10:33:45 +0200

    Edit urls

--- app/src/main/java/eg/edu/eulc/librarysystem/MyApplication.java
+++ app/src/main/java/eg/edu/eulc/librarysystem/MyApplication.java
@@ -25,7 +25,7 @@ public class MyApplication extends Application {
     }
 
     public String getServerName() {
-        String[] servers = {"http://slbapp.kfu.edu.sa/KFU_v6/", "http://www.aradoportal.org.eg/Fuls_v5/", "http://main.eulc.edu.eg/eulc_v5/", "http://library.asu.edu.bh/asu/", "http://library.ku.edu.bh/KUv5/", "http://www.library.su.edu.eg/suV4/", "http://www.nublibrary.com/NUB/", "http://www.nublibrary.com/NUB/", "http://main.eulc.edu.eg/eulc_v5/", "http://main.eulc.edu.eg/eulc_v5/", "http://main.eulc.edu.eg/eulc_v5/", "http://main.eulc.edu.eg/eulc_v5/", "http://main.eulc.edu.eg/eulc_v5/", "http://aljwhara.mans.edu.eg/aljwhara/", "http://schools.mans.edu.eg/schoolsNew/", "http://gocp.mans.edu.eg/", "http://www.eulc.edu.eg/demo/"};
+        String[] servers = {"http://slbapp.kfu.edu.sa/KFU_v6/", "http://www.aradoportal.org.eg/Fuls_v5/", "http://main.eulc.edu.eg/eulc_v5/", "http://library.asu.edu.bh/asu/", "http://library.ku.edu.bh/KUv5/", "http://www.library.su.edu.eg/suV4/", "http://www.nublibrary.com/NUB/", "http://www.nublibrary.com/NUB/", "http://main.eulc.edu.eg/eulc_v5/", "http://main.eulc.edu.eg/eulc_v5/", "http://main.eulc.edu.eg/eulc_v5/", "http://main.eulc.edu.eg/eulc_v5/", "http://main.eulc.edu.eg/eulc_v5/", "http://aljwhara.mans.edu.eg/aljwhara/", "http://schools.mans.edu.eg/schoolsNew/", "http://gocp.mans.edu.eg/Palaces/libraries/", "http://www.eulc.edu.eg/demo/"};
         return servers[sInstance.getApplicationContext().getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE).getInt("site", 16)];
     }
 
